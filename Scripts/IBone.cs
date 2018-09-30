92fa47ec23d2ecc530963c49cb85138454bf9823
ousttrue <ousttrue@gmail.com>
2018-10-01 01:18:31 +0900

    Separated BvhBone.cs

--- Scripts/IBone.cs
+++ Scripts/IBone.cs
@@ -11,4 +11,31 @@ namespace UniHumanoid
         IBone Parent { get; }
         IList<IBone> Children { get; }
     }
+
+    public static class IBoneExtensions
+    {
+        public static IEnumerable<IBone> Traverse(this IBone self)
+        {
+            yield return self;
+            foreach (var child in self.Children)
+            {
+                foreach (var x in child.Traverse())
+                {
+                    yield return x;
+                }
+            }
+        }
+
+        public static Vector3 CenterOfDescendant(this IBone self)
+        {
+            var sum = Vector3.zero;
+            int i = 0;
+            foreach (var x in self.Traverse())
+            {
+                sum += x.SkeletonLocalPosition;
+                ++i;
+            }
+            return sum / i;
+        }
+    }
 }
